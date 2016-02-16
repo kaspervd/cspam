6250a60afd4000e8597061a4b03b7550124b829d
Kasper van Dam <kaspervandam@gmail.com>
2016-02-16 15:33:50 +0100

    Deleted most of the code and restructured useful parts

---
+++ README.rst
@@ -0,0 +1,3 @@
+C-SPAM - version: x.x
+
+Low-frequency data reduction pipeline to be run into CASA
---
+++ cspam/cspam.py
@@ -0,0 +1,5 @@
+from lib import AntennaObjects
+from lib import TableObjects
+from lib import utils
+
+print 'hello world'
